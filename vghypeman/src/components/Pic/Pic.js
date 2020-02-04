2e1147821213ed8b9dd3a092a3774dc59daa5c4b
Matthew Donovan <matthewbryantdonovan@gmail.com>
2020-02-03 22:28:25 -0500

    fixed some css routing/moved asset folder/put wheel on the 'Bus'

--- vghypeman/src/components/Pic/Pic.js
+++ vghypeman/src/components/Pic/Pic.js
@@ -1,5 +1,6 @@
 import $ from 'jquery'
 import Slick from "vue-slick"
+import { EventBus } from "../event-bus";
 
 export default {
   name: 'Pic',
@@ -13,11 +14,11 @@ export default {
       img2: "https://media.rawg.io/media/games/596/596d6cb7e6c53a5f0e1fce018b58137e.jpg",
       img3: "https://media.rawg.io/media/games/596/596d6cb7e6c53a5f0e1fce018b58137e.jpg",
       img4: "https://media.rawg.io/media/games/596/596d6cb7e6c53a5f0e1fce018b58137e.jpg",
-      img5: "https://media.rawg.io/media/games/596/596d6cb7e6c53a5f0e1fce018b58137e.jpg"
+      img5: "https://media.rawg.io/media/games/596/596d6cb7e6c53a5f0e1fce018b58137e.jpg",
+      APIgame: ""
     }
   },
   computed: {
-
   },
   mounted () {
     $(".single-item").slick({
@@ -28,6 +29,11 @@ export default {
       cssEase: 'linear'
   });
 
+  EventBus.$on("clicked-event", game=> {  
+    window.console.log(game);
+    this.APIgame = game;
+
+  });
 
   },
   methods: {
