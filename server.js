ac348e5b1fd57019279e2354b2f465cad40e7a9e
boz7 <zbratkovich@gmail.com>
2020-02-04 20:42:23 -0500

    fix attempt 2

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('./vghypeman/dist'));
+app.use('/', express.static('./vghypeman/dist'));
 
 app.use(routes)
 
