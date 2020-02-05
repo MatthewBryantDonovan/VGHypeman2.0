250d8c84da3890302c441835cd0fd22e12a312d3
Matthew Donovan <matthewbryantdonovan@gmail.com>
2020-02-05 17:31:23 -0500

    profile icon moved to correct area

--- vghypeman/src/App.vue
+++ vghypeman/src/App.vue
@@ -1,29 +1,23 @@
 <template>
   <div id="app">
-     <Profile v-show="ProfileShow"/>
+    <Profile v-show="ProfileShow" />
     <nav id="hi8 valign-wrapper">
-      <button v-on:click="display_profile()">I'm your profile</button>
       <div class="nav-wrapper">
         <a href="/home">
           <div class="left hide-on-small-only" id="VGHlogo"></div>
         </a>
-        <div class="left show-on-small hide-on-med-and-up" id="VGHlogoMobile"></div>
+        <div class="left show-on-small hide-on-med-and-up" id="VGHlogoMobile" href="/home"></div>
         <form action>
           <ul id="nav-mobile" class="right">
             <li>
-              <input
-                id="game-entry"
-                class="form-control"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-                v-on:keyup.enter="getGame()"
-              />
+              <input id="game-entry" class="form-control" type="search" placeholder="Search" aria-label="Search"
+                v-on:keyup.enter="getGame()" />
             </li>
             <li>
-              <button class="searchBtn" type id="game-submit" v-on:click="getGame()"/>
+              <button class="searchBtn" type id="game-submit" v-on:click="getGame()" />
             </li>
             <li>
+              <button v-on:click="display_profile()">file</button>
               <!-- <a class="socialBtn official" href="/myprofile"></a> -->
             </li>
           </ul>
@@ -35,56 +29,57 @@
 </template>
 
 <script>
-import $ from 'jquery';
-// import this anywhere you want to 'Bus' data around
-import { EventBus } from "./components/event-bus";  
-import Profile from './components/Profile/index.vue'
+  import $ from 'jquery';
+  // import this anywhere you want to 'Bus' data around
+  import {
+    EventBus
+  } from "./components/event-bus";
+  import Profile from './components/Profile/index.vue'
 
-export default {
-  name: "app",
-  components: {
-    Profile
-  },
-  data() {
-    return {
-      game: "",
-      ProfileShow: false
-    }
-  },
-  mounted() {
-    EventBus.$on("close-profile", closeProfile => {   
-      this.ProfileShow = closeProfile;
-  });
-  },
-  methods: {
-    getGame: function() {
-      event.preventDefault();
-      this.game = $("#game-entry").val().trim();
-      if (this.game == "") {
-          return;
-      }
-      $("#game-entry").val("");
-      // this.game now has game name for the API
-      // passing it via 'Bus' to Pic,Twitch,Youtube
-      EventBus.$emit("clicked-event", this.game);  
+  export default {
+    name: "app",
+    components: {
+      Profile
     },
-     display_profile: function () {
-      this.ProfileShow = true;
-    }
-  },
-    props: {
-  },
-};
+    data() {
+      return {
+        game: "",
+        ProfileShow: false
+      }
+    },
+    mounted() {
+      EventBus.$on("close-profile", closeProfile => {
+        this.ProfileShow = closeProfile;
+      });
+    },
+    methods: {
+      getGame: function () {
+        event.preventDefault();
+        this.game = $("#game-entry").val().trim();
+        if (this.game == "") {
+          return;
+        }
+        $("#game-entry").val("");
+        // this.game now has game name for the API
+        // passing it via 'Bus' to Pic,Twitch,Youtube
+        EventBus.$emit("clicked-event", this.game);
+      },
+      display_profile: function () {
+        this.ProfileShow = true;
+      }
+    },
+    props: {},
+  };
 </script>
 
 <style>
-@import url('../public/style.css');
+  @import url('../public/style.css');
 
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  #app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 </style>
