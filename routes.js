aa995bcec4088c78bb7cab429d4e69e8ef323fbf
Matthew Donovan <matthewbryantdonovan@gmail.com>
2020-02-05 20:24:42 -0500

    added a few routes

--- routes.js
+++ routes.js
@@ -5,15 +5,91 @@ var db = require("./models");
 
 const express = require('express');
 const router = express.Router();
-router.get("/api/user/:id", function(req, res) {
-      db.user.findOne({
-            where: {
-              id: req.params.id
-            }
-          }).then(function(dbUser) {
-            res.json(dbUser);
-          });
-    });
-  
+
+router.get("/api/user/:id", function (req, res) {
+  db.user.findOne({
+    where: {
+      id: req.params.id
+    }
+  }).then(function (dbUser) {
+    res.json(dbUser);
+  });
+});
+
+router.post("/api/create/profile", function (req, res) {
+  db.user.create(req.body).then(function (dbUser) {
+    res.json(dbUser);
+  });
+});
+
+// PUT route for updating posts
+router.put("/api/update/:id/:type", function (req, res) {
+
+  switch (req.params.type) {
+    case "email":
+      db.user.update(
+        req.body, {
+          where: {
+            id: req.params.id
+          }
+        }).then(function (dbUser) {
+        res.json(dbUser);
+      });
+      break;
+    case "username":
+      db.user.update(
+        req.body, {
+          where: {
+            id: req.params.id
+          }
+        }).then(function (dbUser) {
+        res.json(dbUser);
+      });
+      break;
+    case "password":
+      db.user.update(
+        req.body, {
+          where: {
+            id: req.params.id
+          }
+        }).then(function (dbUser) {
+        res.json(dbUser);
+      });
+      break;
+    case "picture":
+      db.user.update(
+        req.body, {
+          where: {
+            id: req.params.id
+          }
+        }).then(function (dbUser) {
+        res.json(dbUser);
+      });
+      break;
+    case "email":
+      db.user.update(
+        req.body, {
+          where: {
+            id: req.params.id
+          }
+        }).then(function (dbUser) {
+        res.json(dbUser);
+      });
+      break;
+    case "email":
+      db.user.update(
+        req.body, {
+          where: {
+            id: req.params.id
+          }
+        }).then(function (dbUser) {
+        res.json(dbUser);
+      });
+      break;
+
+
+
+  }
+});
 
 module.exports = router;
